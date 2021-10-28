128a180b12d8f652790e01399ce7025f08ad290b
Matias Escobar <escobarmatiash@gmail.com>
2021-10-28 18:57:51 -0300

    2da entrega

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+    return (
+    <header>
+        <h1>NOMBRE DE LA TIENDA</h1>
+         <div class="list-group">
+           <button type="button" class="list-group-item list-group-item-action active" aria-current="true">Opcion 1 </button>
+           <button type="button" class="list-group-item list-group-item-action">Opcion 2</button>
+           <button type="button" class="list-group-item list-group-item-action">Opcion 3</button>
+           <button type="button" class="list-group-item list-group-item-action">Opcion 4</button>
+         </div>
+    </header>
+    )
+}
+
+export default NavBar 
